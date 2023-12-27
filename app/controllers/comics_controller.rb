6874a219974034eda63f55b6195f2560189f8463
karuju <silvertsfye@gmail.com>
2023-12-27 17:42:55 +0900

    comicの基本機能をつけました。要改良

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,4 +1,48 @@
 class ComicsController < ApplicationController
-  def scaffold
+  def index
+  end
+
+  def new
+    @comic = Comic.new
+    @song = Song.find(session[:song_id])
+  end
+
+  def search
+  end
+
+  def show
+    @comic = Comic.find(params[:id])
+  end
+
+  def create
+    @comic = Comic.new(comic_params)
+    if @comic.save
+      session[:comic_id] = @comic.id
+      #redirect_to new_post_path(@comic)
+    else
+      render new
+    end
+
+  end
+
+  def edit
+    @comic = Comic.find_by(comic_params)
+    if @comic.save
+      session[:comic_id] = @comic.id
+      #redirect_to new_post_path(@comic, @song)
+    else
+      render new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def comic_params
+    params.require(:comic).permit(:title, :author, :category, :summary, :uri, :publisher, :published_year)
   end
 end
