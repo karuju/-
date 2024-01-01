71133e271d5d50559f0d489f3fa5745662c69a66
karuju <silvertsfye@gmail.com>
2024-01-01 20:59:36 +0900

    contentテーブルを作成しました

---
+++ db/migrate/20240101114018_content.rb
@@ -0,0 +1,12 @@
+class CreateContents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contents do |t|
+      t.integer :contentable_id, null: false
+      t.string :contentable_type, null: false
+      t.references :post, foreign_key: true
+      t.references :answer, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
