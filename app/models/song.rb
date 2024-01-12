7e9742b0a164b31c08957f1b19284d2847f9a3b7
karuju <silvertsfye@gmail.com>
2024-01-12 17:33:24 +0900

    各モデルのバリデーションを更新しました

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
   validates :name, presence: true
   
-  belongs_to :artist
+  belongs_to :artist, presence: true
   has_many :post_songs
   has_many :posts, through: :post_songs
 
