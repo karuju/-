0c7eba59d7ecdd4132891378dd637e27ef1aba4a
karuju <silvertsfye@gmail.com>
2024-01-25 11:45:12 +0900

    modalのjsをファイルに分けました

--- app/javascript/custom/modal.js
+++ app/javascript/custom/modal.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector("form").addEventListener("submit", function() {
-    var myModal = new bootstrap.Modal(document.getElementById('loadingModal'), {
-      backdrop: 'static',
-      keyboard: false
+  var form = document.querySelector("form");
+  if (form) {
+    form.addEventListener("submit", function() {
+      var myModal = new bootstrap.Modal(document.getElementById('loadingModal'), {
+        backdrop: 'static',
+        keyboard: false
+      });
+      myModal.show();
     });
-    myModal.show();
-  });
+  }
 
-  document.addEventListener("ajax:complete", function() {
+  document.addEventListener("turbolinks:load", function() {
     var myModal = bootstrap.Modal.getInstance(document.getElementById('loadingModal'));
     myModal.hide();
   });
-});
+})
